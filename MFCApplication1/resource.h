75fe4f75173b56d1f4eb9787527d5a231b7cbf2f
yunseyeong <iamsy1207@naver.com>
2016-12-22 23:40:43 +0900

    초기 커밋.

---
+++ MFCApplication1/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication1.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_BUTTON3                     1002
+#define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
+#define IDC_BUTTON7                     1006
+#define IDC_BUTTON8                     1007
+#define IDC_BUTTON9                     1008
+#define IDC_BUTTON10                    1009
+#define IDC_BUTTON11                    1010
+#define IDC_BUTTON12                    1011
+#define IDC_BUTTON13                    1012
+#define IDC_BUTTON14                    1013
+#define IDC_DATETIMEPICKER1             1014
+#define IDC_EDIT1                       1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
